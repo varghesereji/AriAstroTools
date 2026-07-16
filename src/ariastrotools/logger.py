650853dbd17b528c43c833e43860b24071ed69ef
Varghese Reji <varghese@localhost.localdomain>
2026-07-16 22:33:04 +0530

    Now if the package is called directly, the logs will be printed. It also have the setup to write log file if the caller have it's own log file.

--- src/ariastrotools/logger.py
+++ src/ariastrotools/logger.py
@@ -1,4 +1,16 @@
 import logging
+import sys
 
 logger = logging.getLogger("AriAstroTools")
-logger.addHandler(logging.NullHandler())
+
+# If the application hasn't configured logging, log to the console.
+if not logging.getLogger().hasHandlers():
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+    )
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
