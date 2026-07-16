e3764de1a54bda9aea51fcb8237c5b85a252301c
Varghese Reji <varghese@localhost.localdomain>
2026-07-16 22:44:39 +0530

    Logging variance correction.

--- src/ariastrotools/handle_frame.py
+++ src/ariastrotools/handle_frame.py
@@ -111,6 +111,7 @@ def masking_frame(frame, mask, variance=None, method='interpolate'):
         logger.info("Interpolating bad pixels.")
     if variance is not None:
         variance[~mask_bool] = 1000 * variance[~mask_bool]
+        logger.info("Multiplying bad pixel variance with 1000")
         return frame, variance
     return frame
 
