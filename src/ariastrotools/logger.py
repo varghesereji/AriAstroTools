e6bccccec8901d77aceaa3eb01add1e50b7d0d50
Varghese Reji <varghese@localhost.localdomain>
2026-07-16 22:26:37 +0530

    Correct way for logging. Now the Ariastrotools_log.log will not be formed always, when the package is simply called.

--- src/ariastrotools/logger.py
+++ src/ariastrotools/logger.py
@@ -1,25 +1,4 @@
 import logging
-import sys
 
 logger = logging.getLogger("AriAstroTools")
-logger.setLevel(logging.INFO)
-
-# Console handler
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.INFO)
-
-# File handler
-log_filename = "Ariastro_logs.log"
-fh = logging.FileHandler(log_filename)
-fh.setLevel(logging.INFO)
-
-
-# Formatter
-formatter = logging.Formatter(
-    "%(asctime) s -%(name)s - %(levelname)s - %(message)s")
-ch.setFormatter(formatter)
-fh.setFormatter(formatter)
-
-if not logger.hasHandlers():
-    logger.addHandler(ch)
-    logger.addHandler(fh)
+logger.addHandler(logging.NullHandler())
