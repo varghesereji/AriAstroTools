44c35a82b591059f5e279b39c7e231e5c57867f5
Varghese Reji <varghese@localhost.localdomain>
2026-07-20 17:57:32 +0530

    insted of keeping 1 for the tellurics extension, it is replaced with arrray of ones and keep shape the same.

--- src/ariastrotools/instrument.py
+++ src/ariastrotools/instrument.py
@@ -112,7 +112,7 @@ class Handle_NEID:
         corr_var = var / full_tellurics ** 2
         datadict['SCIFLUX'] = corr_flux
         datadict['SCIVAR'] = corr_var
-        datadict['TELLURIC'] = np.array([[[1, 1]]])
+        datadict['TELLURIC'] = np.ones_lise(tellurics)
         return datadict
 
     def barycorr(self, wl_array, header):
