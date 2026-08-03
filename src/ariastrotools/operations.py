f52dee1e3fadb5f70d5d7a92cca38c97539e2a16
Varghese Reji <varghese@localhost.localdomain>
2026-08-03 20:16:43 +0530

    Using logger for table extensions and other extensions

--- src/ariastrotools/operations.py
+++ src/ariastrotools/operations.py
@@ -437,12 +437,14 @@ def combine_data_full(datadict, dataext=[1, 2, 3],
 
     for index, ext in enumerate(extra_keys):
         data = comb_dicts[ext]
+        logger.info("Operation on %s", ext)
         comb_data, _ = combine_data(data,
                                     method=method)
         comb_dicts[ext] = comb_data
     if table_info is not None:
         for extnum, info in table_info.items():
             key = dictkeys[extnum]
+            logger.info("Table operation on %s", key)
             comb_table, _ = combine_bintable(
                 comb_dicts[key],
                 value_cols=info.get("value_cols", []),
