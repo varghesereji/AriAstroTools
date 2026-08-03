0dc6d4cc168a29eb70dc9e5a73e79eb8d83f96a5
Varghese Reji <varghese@localhost.localdomain>
2026-08-03 20:20:20 +0530

    Removed a useless print

--- src/ariastrotools/spectral_utils.py
+++ src/ariastrotools/spectral_utils.py
@@ -230,7 +230,7 @@ def combine_spectra(filesre="*.fits", directory=".",
             telluric_corr = instname_list[1].lower() == 'tel'
         instrument = instrument_dict[instrumentname]()
         fluxext, varext, wlext, extra, tables = instrument.fits_extensions()
-        print(extra, "extra")
+
     req_qtys_dict = defaultdict(list)
     req_qtys_dict_fullext = {}
     for cro, specfile in enumerate(files_list):
